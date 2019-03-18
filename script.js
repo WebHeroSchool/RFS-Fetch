98140168947477be9065868362b3f3a2a8308253
Zaur Zulpikarov <Zulpikarov.Z@gmail.com>
2019-03-18 19:14:47 +0300

    finished the fetch-script

--- script.js
+++ script.js
@@ -1,1 +1,37 @@
-fetch
+    // var url = new URLSearchParams(window.location.search);    
+    // var chosenUser = url.get("username");
+    var chosenUser = "6thSence1";	//defunkt
+    var gitUrl = "https://api.github.com/users/";
+    var FullGitUrl = gitUrl + chosenUser;
+
+
+fetch(FullGitUrl)
+.then(res => res.json())
+.then(json => {
+	console.log(json);
+	if (json.message === "Not Found") {
+	    var error = document.createElement("error");
+	    error.innerHTML = "Информация о пользователе не доступна!";
+	    error.classList.add("err");
+	    document.body.appendChild(error);
+	} else {
+
+		var userProfile = json.html_url;
+
+		var userAvatar = document.createElement("img");
+			userAvatar.src = json.avatar_url;
+			document.body.appendChild(userAvatar);
+
+		var userName = document.createElement("a");
+			userName.href = json.html_url;
+			userName.innerHTML = json.name;
+			document.body.appendChild(userName);
+
+		var userBio = document.createElement("div");
+			userBio.innerHTML = json.bio;
+			document.body.appendChild(userBio);
+	}
+})
+.catch(err => {
+	console.log('Информация о пользователе не доступна');
+})
